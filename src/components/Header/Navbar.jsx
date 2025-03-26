308c04f058ce7ae3257a5d1236dd8915cc01a8dc
hafsaqadar <haf252537@gmail.com>
2025-03-26 11:09:15 +0200

    fixed countrypage nav resp

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -8,14 +8,14 @@ const Navbar = () => {
   return (
    <header className='w-full shadow-sm bg-white dark:bg-dark-element p-8'>
     <nav className='flex items-center justify-between'>
-      <h1 className='text-3xl font-semibold text-light-text dark:text-white'>
+      <h1 className=' text-xl md:text-3xl font-semibold text-light-text dark:text-white'>
         Where in the world?
       </h1>
       <button 
-        className='flex items-center gap-3 text-2xl font-medium text-light-text dark:text-white'
+        className='flex items-center gap-2 md:gap-3 md:text-2xl font-medium text-light-text dark:text-white'
         onClick={toggleTheme}
       >
-        <MdOutlineDarkMode className='text-3xl'/> Dark Mode
+        <MdOutlineDarkMode className='text-xl md:text-3xl'/> Dark Mode
       </button>
     </nav>
    </header>
