64aa642f860c9639480716ea0f823d069a0579a1
hafsaqadar <haf252537@gmail.com>
2025-03-28 11:51:52 +0300

    Update Search.jsx

--- src/components/Hero/Search.jsx
+++ src/components/Hero/Search.jsx
@@ -16,7 +16,7 @@ const Search = () => {
    
   return (
    
-    <div className="flex flex-col lg:flex-row gap-8 lg:justify-between lg:items-center w-full mt-10 px-4">
+    <div className="flex flex-col lg:flex-row gap-8 lg:justify-between lg:items-center w-full mt-16 px-4">
    
     <div className="bg-light-light dark:bg-dark-element flex items-center gap-4 p-4 w-full shadow-md rounded-md lg:w-450 bg-white">
     <IoIosSearch size={29} className='text-light-input dark:text-white' />
@@ -25,7 +25,7 @@ const Search = () => {
     value={searchInput}
     onChange={handleChange}
      placeholder='Search for country...' 
-     className='border-none outline-none px-2 w-full bg-white dark:bg-dark-element  text-light-input dark:text-white text-xl md:text-2xl'/>
+     className='border-none outline-none px-2  py-2 w-full bg-white dark:bg-dark-element  text-light-input dark:text-white text-xl md:text-2xl'/>
     </div>
     
     
